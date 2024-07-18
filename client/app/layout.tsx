63baf47abb4ec3daf6ee7b05cc03b1676fa29f9e
Zero Two <strelizia471@gmail.com>
2024-07-18 20:51:18 +0700

    initial commit

---
+++ client/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "MedCrypt",
+  description: "A healthcare management system for hospitals.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
